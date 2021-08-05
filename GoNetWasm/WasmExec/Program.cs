82d1fb1ec1b135b345b031fa799fe13c04096197
xafero <gberendt@outlook.com>
2021-08-05 04:58:54 +0200

    Adding first code

---
+++ GoNetWasm/WasmExec/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WasmExec
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args == null || args.Length < 1)
+            {
+                Console.WriteLine("usage: " + nameof(WasmExec) + " [wasm binary] [arguments]");
+                return;
+            }
+
+        }
+    }
+}
